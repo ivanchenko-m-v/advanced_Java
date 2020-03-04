aaff8103604179221d919c9d9ea559118273612d
Michael Ivanchenko <misha@kvvaiu.ru>
2020-03-04 23:21:39 +0300

    2020-03-04 +ArrayList

--- lesson01/src/main/java/ru/imv/advancedjava/App.java
+++ lesson01/src/main/java/ru/imv/advancedjava/App.java
@@ -1,13 +1,16 @@
 package ru.imv.advancedjava;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        IntStream.range(0, 100).forEach(i -> list.add(i));
+        list.stream().forEach(integer -> System.out.println(integer));
     }
 }
