8fa6add320c2c60f70c23ab15c96a1e0937fa306
Michael Ivanchenko <misha@kvvaiu.ru>
2020-03-04 23:04:08 +0300

    2020-03-04 first commit

---
+++ lesson01/src/main/java/ru/imv/advancedjava/App.java
@@ -0,0 +1,13 @@
+package ru.imv.advancedjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
