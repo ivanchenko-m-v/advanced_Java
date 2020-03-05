7fe4789574c8a83b497ee469eb9914f92701411a
Michael Ivanchenko <misha@kvvaiu.ru>
2020-03-05 23:29:46 +0300

    2020-03-05 +lesson04

--- lesson03/src/main/java/ru/imv/advancedjava/Lesson03App.java
+++ lesson03/src/main/java/ru/imv/advancedjava/Lesson03App.java
@@ -1,13 +1,26 @@
 package ru.imv.advancedjava;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
  * Hello world!
- *
  */
-public class Lesson03App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Lesson03App {
+    public static void main(String[] args) {
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        measureTime(linkedList);
+        measureTime(arrayList);
+    }
+
+    private static void measureTime(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        IntStream.range(0, 100000).forEach(l -> list.add(0, l));
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 }
