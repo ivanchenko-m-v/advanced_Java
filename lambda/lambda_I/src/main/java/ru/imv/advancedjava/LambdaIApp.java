f9ca0f3348f74c1087efedcf7fcdbad3ddd8a03f
M.Ivanchenko <bearny@ya.ru>
2020-05-04 20:45:13 +0300

    2020-05-04 +lambda parent module

---
+++ lambda/lambda_I/src/main/java/ru/imv/advancedjava/LambdaIApp.java
@@ -0,0 +1,10 @@
+package ru.imv.advancedjava;
+
+/**
+ * Hello world!
+ */
+public class LambdaIApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
